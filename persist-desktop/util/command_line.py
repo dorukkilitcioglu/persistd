c7cf3f8e327a751a08802a9485d11887bed23a14
Doruk Kilitcioglu <doruketc@gmail.com>
2018-07-20 22:24:31 -0400

    Added Windows function for killing mutants

--- persist-desktop/util/command_line.py
+++ persist-desktop/util/command_line.py
@@ -1,4 +1,7 @@
+import os
 import subprocess
+
+from util import const
 
 
 def run_on_command_line(command, input=None):
@@ -27,3 +30,25 @@ def run_on_command_line(command, input=None):
         return sub.returncode, stdout.decode('utf-8'), sub.pid
     except (AttributeError, UnicodeDecodeError) as err:
         return sub.returncode, err, -1
+
+
+def kill_mutant(process_name, object_name):
+    """ Kills a process mutant to make sure everything opens
+    up at the right window.
+    """
+
+    # First, set up the environmental variables that the script needs
+    os.environ[const.ENV_MUTANT_PROCESS_NAME] = process_name
+    os.environ[const.ENV_MUTANT_OBJECT_NAME] = object_name
+
+    # Then, run the actual batch script
+    util_dir = os.path.dirname(os.path.realpath(__file__))
+    file_path = os.path.join(util_dir, 'windows', 'kill_mutant.bat')
+    return_code, _, _ = run_on_command_line([file_path])
+
+    # Even if there was an error, we first unset the environmental variables
+    os.environ[const.ENV_MUTANT_PROCESS_NAME] = ""
+    os.environ[const.ENV_MUTANT_OBJECT_NAME] = ""
+
+    # Return true if there wasn't any errors
+    return return_code == 0
