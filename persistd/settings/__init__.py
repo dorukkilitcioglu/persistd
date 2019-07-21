890df2bedcd512f05a168837dd2d90dddb6c678f
Doruk Kilitcioglu <doruketc@gmail.com>
2019-07-21 11:31:27 -0400

    Made settings json

--- persistd/settings/__init__.py
+++ persistd/settings/__init__.py
@@ -1,16 +1,34 @@
+import json
 import os
-import sys
 
-from persistd.settings.default import *
 from persistd.util.paths import CODE_PATH, SETTINGS_PATH
 from persistd.util.savers import copy_file
 
-DEFAULT_SETTINGS_PATH = os.path.join(CODE_PATH, 'settings', 'default.py')
-LOCAL_SETTINGS_PATH = os.path.join(SETTINGS_PATH, 'local.py')
+DEFAULT_SETTINGS_PATH = os.path.join(CODE_PATH, 'settings', 'default.json')
+LOCAL_SETTINGS_PATH = os.path.join(SETTINGS_PATH, 'local.json')
 
 if not os.path.exists(LOCAL_SETTINGS_PATH):
     copy_file(DEFAULT_SETTINGS_PATH, LOCAL_SETTINGS_PATH)
 
-sys.path.append(os.path.dirname(LOCAL_SETTINGS_PATH))
+BASE_PATH = None
+SUBLIME_TEXT_PATH = None
+CONEMU_PATH = None
+CHROME_PATH = None
 
-from local import *
+with open(os.path.join(DEFAULT_SETTINGS_PATH), 'r') as fp:
+    settings = json.load(fp)
+    BASE_PATH = settings['BASE_PATH']
+    SUBLIME_TEXT_PATH = settings['SUBLIME_TEXT_PATH']
+    CONEMU_PATH = settings['CONEMU_PATH']
+    CHROME_PATH = settings['CHROME_PATH']
+
+with open(os.path.join(LOCAL_SETTINGS_PATH), 'r') as fp:
+    settings = json.load(fp)
+    if 'BASE_PATH' in settings:
+        BASE_PATH = settings['BASE_PATH']
+    if 'SUBLIME_TEXT_PATH' in settings:
+        SUBLIME_TEXT_PATH = settings['SUBLIME_TEXT_PATH']
+    if 'CONEMU_PATH' in settings:
+        CONEMU_PATH = settings['CONEMU_PATH']
+    if 'CHROME_PATH' in settings:
+        CHROME_PATH = settings['CHROME_PATH']
